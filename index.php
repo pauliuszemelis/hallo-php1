2eab088ee34abafef0a86f38423650b26e7182df
Paulius Žemelis <paulius.zemelis@gmail.com>
2017-11-08 20:52:40 +0200

    PHP kintamieji

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$string = "labas";
+$number = 156;
+$real = 13.55;
+$boolean = true;
+$array = ['lt', 'en', 'de'];
+$object = (object)[
+	"key1" => 'lt',
+	"key2" => 'en',
+	"key3" => 'de'];
+
+
+// var_dump($string);
+// var_dump($number);
+// var_dump($real);
+// var_dump($boolean);
+var_dump($array[0]);
+var_dump($object->key2);
+
+echo "<b>Labas PHP</b>";
